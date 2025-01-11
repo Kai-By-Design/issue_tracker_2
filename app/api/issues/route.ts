10ddc0a9602ec57c647fcb2627abaf87f82043ee
Kai <pannu.kai@gmail.com>
2025-01-11 08:18:24 -0800

    Handle Form Submission

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -20,7 +20,7 @@ export async function POST(request: NextRequest) {
   return NextResponse.json(newIssue, { status: 201 });
 }
 
+// import { z } from 'zod';
+// const UserSchema = z.object({ username: z.string().min(5, 'Username must be at least 5 characters'), email: z.string().email('Invalid email format'), password: z.string().min(8, 'Password must contain at least 8 characters'), age: z.number().optional(), }); // Type inference in action const validUserData = { username: 'johnsmith', email: 'john@example.com', password: 'strongpassword123' }; const myUser = UserSchema.parse(validUserData); // TypeScript infers the type of 'myUser' as: // { username: string; email: string; password: string; age?: number }
 
-import { z } from 'zod'; const UserSchema = z.object({ username: z.string().min(5, 'Username must be at least 5 characters'), email: z.string().email('Invalid email format'), password: z.string().min(8, 'Password must contain at least 8 characters'), age: z.number().optional(), }); // Type inference in action const validUserData = { username: 'johnsmith', email: 'john@example.com', password: 'strongpassword123' }; const myUser = UserSchema.parse(validUserData); // TypeScript infers the type of 'myUser' as: // { username: string; email: string; password: string; age?: number }
-
-https://medium.com/@weidagang/zod-schema-validation-made-easy-195f86d82d44
+// https://medium.com/@weidagang/zod-schema-validation-made-easy-195f86d82d44
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,17 +2,47 @@
 
 import { Button, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
 import "easymde/dist/easymde.min.css";
+// import { useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 // import React from "react";
 
+interface IssueForm {
+  title: string;
+  description: string;
+}
+
 const NewIssuePage = () => {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+  // console.log(register("title"));
+
   return (
-    <div className="max-w-xl space-y-3">
-      <TextField.Root placeholder="Title"></TextField.Root>
-      <SimpleMDE placeholder="Description" />
+    <form
+      className="max-w-xl space-y-3"
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/issues", data);
+        router.push("/issues");
+      })}
+    >
+      <TextField.Root
+        placeholder="Title"
+        {...register("title")}
+      ></TextField.Root>
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Description" {...field} />
+        )}
+      />
+      {/* <SimpleMDE placeholder="Description" {...register('description')} /> */}
       <Button>Submit New Issue</Button>
-    </div>
+    </form>
   );
 };
 
 export default NewIssuePage;
+//1:06:47 - ran into zod import error
